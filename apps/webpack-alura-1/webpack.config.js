4126a16aefc6458ad19895ad6951cf2565be2ec8
marco <marcoaureliomunhoz@gmail.com>
2021-06-02 08:46:22 -0300

    .

---
+++ apps/webpack-alura-1/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './app/src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app/dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+            '...'
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/src/index.html',
+            filename: 'index.html',
+            hash: true
+        }),
+        new MiniCssExtractPlugin(),
+        new webpack.optimize.ModuleConcatenationPlugin()
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         { from: './app/src/css', to: 'css' }
+        //     ]
+        // })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 3000
+    }
+};
